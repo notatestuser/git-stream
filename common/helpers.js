90bba8168af11daedcc513ef95dad5db7bdb5172
Paolo Fragomeni <paolo@async.ly>
2012-10-12 11:45:12 +0100

    [lib] pruning and cleaning diff tools

--- common/helpers.js
+++ common/helpers.js
@@ -1,3 +1,4 @@
+
 String.prototype.repeat = function( num ) {
   for( var i = 0, buf = ""; i < num; i++ ) buf += this;
   return buf;
@@ -21,9 +22,34 @@ String.prototype.rjust = function( width, padding ) {
     return this.toString();
 };
 
+var forEach = Array.prototype.forEach;
+var breaker = {};
+
 module.exports = {
 
   times: function(n, iterator, context) {
     for (var i = 0; i < n; i++) iterator.call(context, i);
-  }
+  },
+  
+  each: function(obj, iterator, context) {
+
+    if (obj == null) { return; }
+    
+    if (forEach && obj.forEach === forEach) {
+
+      obj.forEach(iterator, context);
+    } 
+    else if (obj.length === +obj.length) {
+
+      for (var i = 0, l = obj.length; i < l; i++) {
+        if (iterator.call(context, obj[i], i, obj) === breaker) { return; }
+      }
+    } else {
+      for (var key in obj) {
+        if (_.has(obj, key)) {
+          if (iterator.call(context, obj[key], key, obj) === breaker) { return; }
+        }
+      }
+    }
+  };
 };
