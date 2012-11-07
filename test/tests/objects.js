5cd890e3697a8a0353b5de186d657acd739580e2
Elijah Insua <tmpvar@gmail.com>
2012-11-07 15:35:55 -0700

    [test] add messages to asserts; improve t.plan() for tree creation test

--- test/tests/objects.js
+++ test/tests/objects.js
@@ -16,7 +16,7 @@ module.exports = {
   "objects: creation (invalid type)" : function(t) {
     var obj = objects.createObject('bla');
 
-    t.equal(obj.type, objects.BAD);
+    t.equal(obj.type, objects.BAD, 'should be of type "bad" (-1)');
     t.end();
   },
 
@@ -24,13 +24,13 @@ module.exports = {
 
     var commit = objects.createObject(objects.COMMIT, commitParts.join('\n'));
 
-    t.equal(commit.tree, '039486ad38f1b796b5cb39b2548481016af44d8e');
-    t.equal(commit.parents.length, 1);
-    t.equal(commit.parents[0], '0af4f50f39cbd756d0b5c6da097ad857479c083e');
-    t.equal(commit.author.name, 'Elijah Insua');
-    t.equal(commit.author.email, 'tmpvar@gmail.com');
-    t.equal(commit.author.timestamp, 1352145386);
-    t.equal(commit.author.timezone, '-0700');
+    t.equal(commit.tree, '039486ad38f1b796b5cb39b2548481016af44d8e', 'tree sha matches');
+    t.equal(commit.parents.length, 1, 'parent length is the same');
+    t.equal(commit.parents[0], '0af4f50f39cbd756d0b5c6da097ad857479c083e', 'parent sha matches');
+    t.equal(commit.author.name, 'Elijah Insua', 'author matches');
+    t.equal(commit.author.email, 'tmpvar@gmail.com', 'author email matches');
+    t.equal(commit.author.timestamp, 1352145386, 'timestamp matches');
+    t.equal(commit.author.timezone, '-0700', 'timezone matches');
 
     t.end();
   },
@@ -40,39 +40,47 @@ module.exports = {
     var commit = objects.createObject('commit');
     var commit2 = objects.createObject(objects.COMMIT);
 
-    t.equal(commit.type, commit2.type);
+    t.equal(commit.type, commit2.type, 'types match');
     t.end();
   },
 
   "objects [commit]: creation (numeric) type)" : function(t) {
 
     var commit = objects.createObject(objects.COMMIT);
-    var commit2 = objects.createObject(objects.COMMIT);
 
-    t.equal(commit.type, commit2.type);
+    t.equal(commit.type, objects.COMMIT, 'types match');
     t.end();
   },
 
   "objects [commit]: serialization" : function(t) {
     var commitString = commitParts.join('\n');
     var commit = objects.createObject(objects.COMMIT, commitString);
-    t.equal(commit.toString(), commitString);
+    t.equal(commit.toString(), commitString, 'commit strings match');
 
     t.end();
   },
   "objects [tree]: creation" : function(t) {
-    t.plan(1)
+
     repo.getPackFile(function(err, packFile) {
-      t.ok(!err);
+
+      var plan = 1;
+      packFile.verifyObjs.forEach(function(obj) {
+        if (obj.type === 'tree') {
+          plan+=2;
+        }
+      });
+
+      t.plan(plan);
+      t.ok(!err, 'no error');
 
       pack.unpack(packFile.buffer, function(err, obj) {
-        t.end();
       }, function make(type, data) {
+
         if (type === objects.TREE) {
           var tree = objects.createObject(type, data);
 
-          t.equal(tree.type, objects.TREE);
-          t.ok(tree.nodes.length > 0);
+          t.equal(tree.type, objects.TREE, 'it is infact a tree');
+          t.ok(tree.nodes.length > 0, 'number of nodes match');
 
           return tree;
         }
